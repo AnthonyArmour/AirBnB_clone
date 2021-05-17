0178b6b4959f0d2a608530a3cdc3b983c28e84f4
Jay Calhoun <jwcalhoun2@gmail.com>
2021-05-17 19:00:54 +0000

    Adding test directory with first test file

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""A module for unittests for the base model class"""
+
+
+import unittest
+from models.base_model import BaseModel
+
+
+class TestBaseModel(unittest.TestCase):
+    """A class to test BaseModel"""
+
+if __name__ == '__main__':
+    unittest.main()
