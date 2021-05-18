0e434c88d14616f6536363ef3d0814b1190d49f1
Jay Calhoun <jwcalhoun2@gmail.com>
2021-05-18 18:09:03 +0000

    adding tests for file storage with base model

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -3,12 +3,49 @@
 
 
 import unittest
+import os
+from models.engine.file_storage import FileStorage
 from datetime import datetime
 from models.base_model import BaseModel
 
 
+fs = FileStorage()
+
+
 class TestBaseModel(unittest.TestCase):
     """A class to test BaseModel"""
+
+    def test_filestorage_reload(self):
+        bm1 = BaseModel()
+        bm1.save()
+        dic = fs._FileStorage__objects
+        bm1.my_number = 89
+        self.assertEqual(bm1.my_number, 89)
+        fs.reload()
+        self.assertEqual(dic, fs._FileStorage__objects)
+
+    def test_filestorage_save(self):
+        bm1 = BaseModel()
+        bm1.save()
+        self.assertNotEqual(os.path.getsize(fs._FileStorage__file_path), 0)
+
+
+    def test_filestorage_new(self):
+        bm1 = BaseModel()
+        self.assertNotEqual(len(fs._FileStorage__objects), 0)
+
+    def test_filestorage_all(self):
+        bm1 = BaseModel()
+        self.assertIsInstance(fs.all(), dict)
+
+    def test_filestorage_objects(self):
+        bm1 = BaseModel()
+        self.assertIsInstance(fs._FileStorage__objects, dict)
+
+    def test_file_exists(self):
+        bm1 = BaseModel()
+        bm1.save()
+        self.assertTrue(os.path.exists(fs._FileStorage__file_path))
 
     def test_id_assignment(self):
         bm1 = BaseModel()
