39f7f677c7f4d6dfdb32063cbd060d55dd54b238
AnthonyArmour <anthonydarmour@gmail.com>
2021-05-18 04:46:25 +0000

    added filestorage class and linked to basemodels class

---
+++ models/engine/file_storage.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Serializes and deserializes json files"""
+
+import json
+import os
+
+
+class FileStorage():
+    """serializes and deserializes json file"""
+    __file_path = "HBNB_Listings.json"
+    __objects = dict()
+
+    def all(self):
+        """returns dictionary of objects"""
+        return self.__objects
+
+    def new(self, obj):
+        """sets in __objects the obj"""
+        obj_key = obj["__class__"]
+        obj_key = obj_key + "." + obj["id"]
+        FileStorage.__objects[obj_key] = dict(obj)
+
+    def save(self):
+        """serializes object to the json file"""
+        with open(FileStorage.__file_path, 'w') as fh:
+            fh.write(json.dumps([FileStorage.__objects]))
+
+    def reload(self):
+        """deserializes json file to __objects"""
+        if os.path.exists(FileStorage.__file_path):
+            with open(FileStorage.__file_path, 'r') as fh:
+                objs_str = fh.read()
+            obj_lst = json.loads(objs_str)
+            print(obj_lst)
