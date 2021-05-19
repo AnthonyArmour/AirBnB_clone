134a12043d372a16aa707f626925c039c66c625a
AnthonyArmour <anthonydarmour@gmail.com>
2021-05-19 18:29:06 +0000

    fixed int and float attr tests for place class

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -45,42 +45,42 @@ class TestUser(unittest.TestCase):
         if os.path.exists(fs._FileStorage__file_path):
             os.remove(fs._FileStorage__file_path)
         u1 = Place()
-        u1.number_rooms = "blah"
+        u1.number_rooms = 3
         self.assertIsInstance(u1.number_rooms, int)
 
     def test_number_bathrooms(self):
         if os.path.exists(fs._FileStorage__file_path):
             os.remove(fs._FileStorage__file_path)
         u1 = Place()
-        u1.number_bathrooms = "blah"
+        u1.number_bathrooms = 3
         self.assertIsInstance(u1.number_bathrooms, int)
 
     def test_max_guest(self):
         if os.path.exists(fs._FileStorage__file_path):
             os.remove(fs._FileStorage__file_path)
         u1 = Place()
-        u1.max_guest = "blah"
+        u1.max_guest = 4
         self.assertIsInstance(u1.max_guest, int)
 
     def test_price_by_night(self):
         if os.path.exists(fs._FileStorage__file_path):
             os.remove(fs._FileStorage__file_path)
         u1 = Place()
-        u1.price_by_night = "blah"
+        u1.price_by_night = 5
         self.assertIsInstance(u1.price_by_night, int)
 
     def test_latitude(self):
         if os.path.exists(fs._FileStorage__file_path):
             os.remove(fs._FileStorage__file_path)
         u1 = Place()
-        u1.latitude = "blah"
+        u1.latitude = 4.333
         self.assertIsInstance(u1.latitude, float)
 
     def test_lobgitude(self):
         if os.path.exists(fs._FileStorage__file_path):
             os.remove(fs._FileStorage__file_path)
         u1 = Place()
-        u1.longitude = "blah"
+        u1.longitude = 4.33
         self.assertIsInstance(u1.longitude, float)
 
     def test_amenity_ids(self):
