c8896f32b91af773b1edd573b333f7c6d1ec2472
AnthonyArmour <anthonydarmour@gmail.com>
2021-05-19 21:29:34 +0000

    debug try for user test

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -15,32 +15,26 @@ class TestUser(unittest.TestCase):
 
     """class to test User"""
 
+    u1 = User()
+
     def test_email(self):
         if os.path.exists(fs._FileStorage__file_path):
             os.remove(fs._FileStorage__file_path)
-        u1 = User()
-        u1.email = "blah"
         self.assertIsNot(u1.email, None)
 
     def test_password(self):
         if os.path.exists(fs._FileStorage__file_path):
             os.remove(fs._FileStorage__file_path)
-        u1 = User()
-        u1.password = "blah"
         self.assertIsNot(u1.password, None)
 
     def test_first_name(self):
         if os.path.exists(fs._FileStorage__file_path):
             os.remove(fs._FileStorage__file_path)
-        u1 = User()
-        u1.first_name = "blah"
         self.assertIsNot(u1.first_name, None)
 
     def test_last_name(self):
         if os.path.exists(fs._FileStorage__file_path):
             os.remove(fs._FileStorage__file_path)
-        u1 = User()
-        u1.last_name = "blah"
         self.assertIsNot(u1.last_name, None)
 
 if __name__ == '__main__':
